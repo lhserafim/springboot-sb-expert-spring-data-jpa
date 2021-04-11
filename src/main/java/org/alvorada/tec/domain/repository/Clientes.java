6a2f2e1b1bcdeed7c9fea726f05c92a7a421882b
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-04-11 18:47:56 -0300

    Refatorando o código para trabalhar com JPA

--- src/main/java/org/alvorada/tec/domain/repository/Clientes.java
+++ src/main/java/org/alvorada/tec/domain/repository/Clientes.java
@@ -2,27 +2,15 @@ package org.alvorada.tec.domain.repository;
 
 import org.alvorada.tec.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class Clientes {
-
-    // Constantes em caixa ALTA
-    private static String INSERT = "INSERT INTO cliente (nome) values (?)"; // ? é um placeholder
-    private static String UPDATE = "UPDATE cliente SET nome = ? WHERE id = ?";
-    private static String DELETE = "DELETE FROM cliente WHERE id = ?";
-    private static String SELECT_ALL = "SELECT * FROM cliente";
-
-    @Autowired // p/ fazer a injeção
-    private JdbcTemplate jdbcTemplate; // Usando pois não estou com o JPA configurado
 
     @Autowired // p/ fazer a injeção (IOC)
     private EntityManager entityManager;
@@ -34,43 +22,40 @@ public class Clientes {
         return cliente;
     }
 
+    @Transactional
     public Cliente atualizar(Cliente cliente) {
-        // Os parametros precisam estar em ordem
-        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        entityManager.merge(cliente);
         return cliente;
     }
 
+    @Transactional
     public void deletar(Cliente cliente) {
-        // Os parametros precisam estar em ordem
-        jdbcTemplate.update(DELETE, new Object[]{cliente.getId()});
+        //Para evitar o erro: Removing a detached instance, que acontece quando a entidade está transiente, devemos fazer o merge
+        //Se o entityManager não contiver estea instancia de cliente, ou seja se não estiver sincronizado, fazer o merge
+        if(!entityManager.contains(cliente)) {
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> obterTodos() {
-        // Recebe a query e um RowMapper. O RowMapper mapeia as colunas do resultado da query para a classe informada
-        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+        return entityManager.createQuery("FROM Cliente", Cliente.class).getResultList();
     }
 
+    // usando o readOnly a pesquisa fica mais rápida pq o JPA não faz cache
+    @Transactional(readOnly = true)
     public List<Cliente> buscarPorNome(String nome) {
-        // Usando o .concat() ao invés de criar uma constante de select a parte
-        return jdbcTemplate.query(
-                SELECT_ALL.concat(" WHERE nome LIKE ? "),
-                new Object[]{"%" + nome + "%"}, // fazendo like
-                obterClienteMapper());
+        String jpql = "SELECT c FROM Cliente c WHERE c.nome LIKE :nome";
+        // Como eu tenho parâmetro, eu preciso criar uma query tipada, passando a query jpql e a classe que ele vai retornar
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
     }
 
-    // Este método foi criado pois como eu estou reutilizando o RowMapper, nada melhor do que separá-lo em um método
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                // Setando os valores retornados em variáveis p/ passar p/ o construtor
-                Integer id = resultSet.getInt("id");
-                String nome = resultSet.getString("nome");
-                // Pegar a coluna desejada do resultSet e mapeá-la na nova instancia, com base no construtor da classe
-                return new Cliente(id, nome);
-            }
-        };
+    @Transactional(readOnly = true)
+    public Cliente buscarPorId(Integer id) {
+        return entityManager.find(Cliente.class, id);
     }
-
 
 }
